84eb43e9808d1ce9fd093ae1bf1a75e5c0840443
Aurélien Barre <aurelien.barre@parrot.com>
2015-08-19 17:38:22 +0200

    [DEV] update atom.mk
    Change-Id: Id8e0c4d2068d847daa015a7f81e6951023e8f435

--- atom.mk
+++ atom.mk
@@ -8,6 +8,12 @@ LOCAL_DESCRIPTION := H.264 Elementary Stream Tools Library
 
 LOCAL_LIBRARIES := libARSAL libARStream
 
+LOCAL_INSTALL_HEADERS := \
+    include/beaver/beaver.h:usr/include/beaver/ \
+    include/beaver/beaver_filter.h:usr/include/beaver/ \
+    include/beaver/beaver_parser.h:usr/include/beaver/ \
+    include/beaver/beaver_parrot.h:usr/include/beaver/
+
 LOCAL_CFLAGS := -Wextra
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SRC_FILES := src/beaver_filter.c src/beaver_parser.c src/beaver_parrot.c
