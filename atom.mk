91a821cd84bc6bc59a2a602fcd6984070a3a0432
Aurélien Barre <aurelien.barre@parrot.com>
2015-08-19 10:30:31 +0200

    [DEV] add atom.mk
    Change-Id: I90bba1d7fb78b61917efdbeae35fc860c0d8827f

---
+++ atom.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CATEGORY_PATH := dragon/libs
+LOCAL_MODULE := libBeaver
+LOCAL_DESCRIPTION := H.264 Elementary Stream Tools Library
+
+LOCAL_LIBRARIES := libARSAL libARStream
+LOCAL_EXPORT_LDLIBS := -lbeaver
+
+LOCAL_CFLAGS := -Wextra
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := src/beaver_filter.c src/beaver_parser.c
+include $(BUILD_LIBRARY)
+
