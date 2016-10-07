3b043809e8e2e80dd37d4e1bfe0df0f08649fa18
Aurélien Barre <aurelien.barre@parrot.com>
2016-10-07 09:48:43 +0200

    [DEV] video_stats script: support simple stats and file output
    Change-Id: I1ac32266cc73524d1b0de13360161c76ad869714

--- tools/scripts/stream_video_stats.py
+++ tools/scripts/stream_video_stats.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def videoStats(inFile):
+def videoStats(inFile, outFile):
     fig1 = plt.figure()
     plt.subplots_adjust(left=0.03, right=0.97, bottom=0.03, top=0.92)
     f = open(inFile, 'r')
@@ -398,37 +398,154 @@ def videoStats(inFile):
 
 
     plt.draw()
-    plt.show()
+    if outFile != '':
+        plt.savefig(outFile)
+    else:
+        plt.show()
+
+
+def simpleVideoStats(inFile, outFile):
+    fig1 = plt.figure()
+    plt.subplots_adjust(left=0.03, right=0.97, bottom=0.03, top=0.92)
+    f = open(inFile, 'r')
+    firstLine = f.readline()
+    f.close()
+    if firstLine != '' and firstLine[0] == '#':
+        title = firstLine[1:]
+        title = title.strip()
+    else:
+        title = inFile
+    plt.suptitle('Video stats (' + title + ')', fontsize=16, color='0.4')
+
+    data = pd.read_csv(inFile, sep=' ', comment='#', skip_blank_lines=True)
+
+
+    # for compatibility with old stats files
+    if 'erroredSecondCount' in data.columns:
+        lblEsr = 'erroredSecondCount'
+        lblEsrZ0 = 'erroredSecondCountByZone[0]'
+        lblEsrZ1 = 'erroredSecondCountByZone[1]'
+        lblEsrZ2 = 'erroredSecondCountByZone[2]'
+        lblEsrZ3 = 'erroredSecondCountByZone[3]'
+        lblEsrZ4 = 'erroredSecondCountByZone[4]'
+    else:
+        lblEsr = 'errorSecondCount'
+        lblEsrZ0 = 'errorSecondCountByZone[0]'
+        lblEsrZ1 = 'errorSecondCountByZone[1]'
+        lblEsrZ2 = 'errorSecondCountByZone[2]'
+        lblEsrZ3 = 'errorSecondCountByZone[3]'
+        lblEsrZ4 = 'errorSecondCountByZone[4]'
+    if 'erroredOutputFrameCount' in data.columns:
+        lblErroredOutputFrameCount = 'erroredOutputFrameCount'
+    else:
+        lblErroredOutputFrameCount = ''
+
+
+    # overall frames
+    overallTotalFrameCount = float(data['totalFrameCount'].iat[-1])
+    if lblErroredOutputFrameCount != '':
+        overallErroredOutputFrameCount = float(data[lblErroredOutputFrameCount].iat[-1])
+    else:
+        overallErroredOutputFrameCount = 0
+    overallNoErrorOutputFrameCount = float(data['outputFrameCount'].iat[-1]) - overallErroredOutputFrameCount
+    overallDiscardedFrameCount = float(data['discardedFrameCount'].iat[-1])
+    overallMissedFrameCount = float(data['missedFrameCount'].iat[-1]) - overallDiscardedFrameCount
+    overallTotalFrameCount2 = overallErroredOutputFrameCount + overallNoErrorOutputFrameCount + overallDiscardedFrameCount + overallMissedFrameCount
+    if overallTotalFrameCount2 != overallTotalFrameCount:
+        print "Warning - overall frame count missmatch: " + str(int(overallTotalFrameCount)) + " vs. " + str(int(overallTotalFrameCount2))
+
+    axOverallFrames = fig1.add_subplot(2, 2, 1)
+    axOverallFrames.set_title("Overall frames", color='0.4')
+    axOverallFrames.pie([overallNoErrorOutputFrameCount, overallErroredOutputFrameCount, overallDiscardedFrameCount, overallMissedFrameCount], labels=['Output (no errors)', 'Output (with errors)', 'Discarded', 'Missed'], colors=['lightgreen', 'cadetblue', 'salmon', 'firebrick'], autopct='%1.1f%%', startangle=90)
+    axOverallFrames.set_aspect('equal')
+
+
+    # ESR by zone
+    totalTime = float(data['timestamp'].iat[-1] - data['timestamp'].iat[0] + 1000000) / 1000000.
+    zoneEsr = [float(data[lblEsrZ4].iat[-1]) / totalTime * 100., float(data[lblEsrZ3].iat[-1]) / totalTime * 100., float(data[lblEsrZ2].iat[-1]) / totalTime * 100., float(data[lblEsrZ1].iat[-1]) / totalTime * 100., float(data[lblEsrZ0].iat[-1]) / totalTime * 100.]
+    zoneEsrYPos = np.arange(5)
+    zoneEsrLabel = ('5', '4', '3', '2', '1')
+
+    axZoneEsr = fig1.add_subplot(2, 2, 2)
+    axZoneEsr.set_xlim(0, 100)
+    axZoneEsr.set_title("ESR by zone", color='0.4')
+    axZoneEsr.barh(zoneEsrYPos, zoneEsr, align='center', alpha=0.8, color='cadetblue')
+    axZoneEsr.set_yticks(zoneEsrYPos)
+    axZoneEsr.set_yticklabels(zoneEsrLabel)
+    axZoneEsr.set_xlabel('ESR (%)')
+    axZoneEsr.set_ylabel('Zone')
+
+
+    # overall macroblocks
+    overallUnknownMbCount = float(data['macroblockStatus[0][0]'].iat[-1] + data['macroblockStatus[0][1]'].iat[-1] + data['macroblockStatus[0][2]'].iat[-1] + data['macroblockStatus[0][3]'].iat[-1] + data['macroblockStatus[0][4]'].iat[-1])
+    overallValidISliceMbCount = float(data['macroblockStatus[1][0]'].iat[-1] + data['macroblockStatus[1][1]'].iat[-1] + data['macroblockStatus[1][2]'].iat[-1] + data['macroblockStatus[1][3]'].iat[-1] + data['macroblockStatus[1][4]'].iat[-1])
+    overallValidPSliceMbCount = float(data['macroblockStatus[2][0]'].iat[-1] + data['macroblockStatus[2][1]'].iat[-1] + data['macroblockStatus[2][2]'].iat[-1] + data['macroblockStatus[2][3]'].iat[-1] + data['macroblockStatus[2][4]'].iat[-1])
+    overallMissingConcealedMbCount = float(data['macroblockStatus[3][0]'].iat[-1] + data['macroblockStatus[3][1]'].iat[-1] + data['macroblockStatus[3][2]'].iat[-1] + data['macroblockStatus[3][3]'].iat[-1] + data['macroblockStatus[3][4]'].iat[-1])
+    overallMissingMbCount = float(data['macroblockStatus[4][0]'].iat[-1] + data['macroblockStatus[4][1]'].iat[-1] + data['macroblockStatus[4][2]'].iat[-1] + data['macroblockStatus[4][3]'].iat[-1] + data['macroblockStatus[4][4]'].iat[-1])
+    overallErrorPropagationMbCount = float(data['macroblockStatus[5][0]'].iat[-1] + data['macroblockStatus[5][1]'].iat[-1] + data['macroblockStatus[5][2]'].iat[-1] + data['macroblockStatus[5][3]'].iat[-1] + data['macroblockStatus[5][4]'].iat[-1])
+    overallValidTotalMbCount = overallValidISliceMbCount + overallValidPSliceMbCount
+    overallInvalidTotalMbCount = overallUnknownMbCount + overallMissingConcealedMbCount + overallMissingMbCount + overallErrorPropagationMbCount
+    overallTotalMbCount = overallValidTotalMbCount + overallInvalidTotalMbCount
+
+    axOverallMbs = fig1.add_subplot(2, 2, 3)
+    axOverallMbs.set_title("Overall macroblocks", color='0.4')
+    axOverallMbs.pie([overallValidISliceMbCount, overallValidPSliceMbCount, overallMissingConcealedMbCount, overallMissingMbCount, overallErrorPropagationMbCount, overallUnknownMbCount], labels=['Valid (I)', 'Valid (P)', 'Missing (concealed)', 'Missing', 'Error propagation', 'Unknown'], colors=['mediumseagreen', 'lightgreen', 'salmon', 'firebrick', 'cadetblue', '0.3'], autopct='%1.1f%%', startangle=90)
+    axOverallMbs.set_aspect('equal')
+
+    axOverallErrorMbs = fig1.add_subplot(2, 2, 4)
+    axOverallErrorMbs.set_title("Overall error macroblocks", color='0.4')
+    axOverallErrorMbs.pie([overallMissingConcealedMbCount, overallMissingMbCount, overallErrorPropagationMbCount, overallUnknownMbCount], labels=['Missing (concealed)', 'Missing', 'Error propagation', 'Unknown'], colors=['salmon', 'firebrick', 'cadetblue', 'k'], autopct='%1.1f%%', startangle=90)
+    axOverallErrorMbs.set_aspect('equal')
+
+
+    plt.draw()
+    if outFile != '':
+        plt.savefig(outFile)
+    else:
+        plt.show()
 
 
 def usage():
-    print 'Usage:'
-    print '    ' + sys.argv[0] + ' -i <input_file>'
+    print "Usage:"
+    print "    " + sys.argv[0] + " -i | --infile <input_file>"
+    print "Options:"
+    print "    -o | --outfile <output_file>    Output to file instead of GUI"
+    print "    -h | --help                     Print this message"
+    print "    -s | --simple                   Simple output (less graphs)"
 
 
 def main(argv):
     inFile = ''
+    outFile = ''
+    simple = False
 
     # command-line arguments
     try:
-        opts, args = getopt.getopt(argv,"hi:",["infile="])
+        opts, args = getopt.getopt(argv,"hsi:o:",["help", "infile=", "outfile=", "simple"])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
 
     for opt, arg in opts:
-        if opt == '-h':
+        if opt in ("-h", "--help"):
             usage()
             sys.exit()
         elif opt in ("-i", "--infile"):
             inFile = arg
+        elif opt in ("-o", "--outfile"):
+            outFile = arg
+        elif opt in ("-s", "--simple"):
+            simple = True
 
     if inFile == '':
         usage()
         sys.exit(2)
 
     if inFile != '':
-        videoStats(inFile)
+        if simple:
+            simpleVideoStats(inFile, outFile)
+        else:
+            videoStats(inFile, outFile)
 
 
 if __name__ == '__main__':
